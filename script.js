1cdfe6baaa23ab8dd80fcdf03ae799c744c0f2e4
Klaus Bisanz <klaus.bisanz@accenture.com>
2021-03-08 09:58:00 +0100

    Scrollbar changed

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ function scrollFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.height = scrolled + "%";
+  document.getElementById("myBar").style.width = scrolled + "%";
 }
