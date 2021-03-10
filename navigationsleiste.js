0e466487dfe6274d8ee20c3885564297705d86e9
Clemens Juettner <clemens.juettner@accenture.com>
2021-03-10 18:13:01 +0100

    styles and content needed

--- navigationsleiste.js
+++ navigationsleiste.js
@@ -4,4 +4,5 @@ function scrollFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";}
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
--- quiz.js
+++ quiz.js
@@ -1,5 +1,5 @@
-const gameTypes = ["+", "-", "*", "/"];
-
+const questions = ["Wer ist CEO von Accenture?", "Wo liegt der Hauptsitz von Accenture?", "Wie lautet das Kürzel von Accenture an der New York Stock Exchange?", "Wo liegt der Hauptsitz innerhalb von Deutschland?"];
+const answers = ["Julie Sweet", "Dublin", "ACN", "Kronberg"]
 
 document.addEventListener("DOMContentLoaded", function() {
     play();
@@ -11,72 +11,28 @@ function play() {
 }
 
 function newTry() {
-    let gameType = getGametype();
-    runGame(gameType);
+    let question = getQuestion();
+    runGame(question);
 }
 
 function newTry1() {
-    let gameType1 = getGametype();
-    runGame1(gameType1);
+    let question1 = getQuestion();
+    runGame1(question1);
 }
-function getGametype() {
+function getQuestion() {
     let index = Math.floor(Math.random() * 4);
-    return gameTypes[index];
+    return questions[index];
 }
-function runGame(gameType) {
-    
-    let num1 = getRandomNumber();
-    let num2 = getRandomNumber();
-
-    switch(gameType) {
-        case "+":
-            showAddQuestion(num1, num2);
-            break;
-        case "-":
-            showSubQuestion(num1, num2);
-            break;
-        case "*":
-            showMulQuestion(num1, num2);
-            break;
-        case "/":
-            showDivQuestion(num1, num2);
-            break;
-        default: 
-            alert('Unbekannte Operation: ${gametype}');   
-    }
+function runGame(question) {
+   showQuestion(question);
 }
-function runGame1(gameType1) {
-
-    let num3 = getRandomNumber();
-    let num4 = getRandomNumber();
-
-    switch(gameType1) {
-        case "+":
-            showAddQuestion1(num3, num4);
-            break;
-        case "+":
-            showSubQuestion1(num3, num4);
-            break;
-        case "+":
-            showMulQuestion1(num3, num4);
-            break;
-        case "+":
-            showDivQuestion1(num3, num4);
-            break;
-        default: 
-            alert('Unbekannte Operation: ${gametype}');
-    }
-        
-}
-
-function getRandomNumber() {
-    let randomNumber = Math.floor(Math.random() * 1000) + 1;
-    return randomNumber;
+function runGame1(question1) {
+   showQuestion1(question1);    
 }
 
 function validateAnswer(event) {
     event.preventDefault();
-    let userAnswer = parseInt(document.getElementById("antwortBox").value);
+    let userAnswer = document.getElementById("antwortBox").value;
     let rightAnswer = calculateAnswer();
     let isCorrect = userAnswer === rightAnswer;
 
@@ -88,49 +44,32 @@ function validateAnswer(event) {
     
 }
 function validateAnswer1(event) {
-    let userAnswer1 = parseInt(document.getElementById("antwortBox2").value);
+    event.preventDefault();
+    let userAnswer1 = document.getElementById("antwortBox2").value;
     let rightAnswer1 = calculateAnswer1();
     let isCorrect1 = userAnswer1 === rightAnswer1;
 
     if (isCorrect1) {
-        this.addEventListener('submit1', showContent1);
+        this.addEventListener('submit', showContent1);
     } else {
         newTry1();
     }
 }
 
 function calculateAnswer() {
-    let operand1 = parseInt(document.getElementById("operand1").innerText);
-    let operand2 = parseInt(document.getElementById("operand2").innerText);
-    let operator = document.getElementById("operator").innerText;
-
-    switch(operator) {
-        case "+":
-            return operand1 + operand2;
-        case "-":
-            return operand1 - operand2;     
-        case "*":
-            return operand1 * operand2;
-        case "/":
-            return operand1 / operand2;    
-    } 
+    for (let i = 0; i <= questions.length; i ++) {
+        if (document.getElementById("question").innerHTML === questions[i]) {
+        return answers[i];  
+        }
+    }  
 }
 
 function calculateAnswer1() {
-    let operand3 = parseInt(document.getElementById("operand3").innerText);
-    let operand4 = parseInt(document.getElementById("operand4").innerText);
-    let operator2 = document.getElementById("operator2").innerText;
-
-    switch(operator2) {
-        case "+":
-            return operand3 + operand4;
-        case "-":
-            return operand3 - operand4;     
-        case "*":
-            return operand3 * operand4;
-        case "/":
-            return operand3 / operand4;
-    }
+    for (let i = 0; i <= questions.length; i ++) {
+        if (document.getElementById("question1").innerHTML === questions[i]) {
+        return answers[i];  
+        }
+    } 
 }
 
 function showContent() {
@@ -143,49 +82,12 @@ function showContent1() {
     card1.classList.add('flip');
 }
 
-function showAddQuestion(operand1, operand2) {
-    document.getElementById("operand1").textContent = operand1;
-    document.getElementById("operand2").textContent = operand2;
-    document.getElementById("operator").textContent = "+";
+function showQuestion(question) {
+    document.getElementById("question").textContent = question;
+   
 }
 
-function showSubQuestion(operand1, operand2) {
-    document.getElementById("operand1").textContent = operand1;
-    document.getElementById("operand2").textContent = operand2;
-    document.getElementById("operator").textContent = "-";
-}
-
-function showMulQuestion(operand1, operand2) {
-    document.getElementById("operand1").textContent = operand1;
-    document.getElementById("operand2").textContent = operand2;
-    document.getElementById("operator").textContent = "*";
-}
-
-function showDivQuestion(operand1, operand2) {
-    document.getElementById("operand1").textContent = operand1;
-    document.getElementById("operand2").textContent = operand2;
-    document.getElementById("operator").textContent = "/";
-}
-function showAddQuestion1(operand3, operand4) {
-    document.getElementById("operand3").textContent = operand3;
-    document.getElementById("operand4").textContent = operand4;
-    document.getElementById("operator2").textContent = "+";
-}
-
-function showSubQuestion1(operand3, operand4) {
-    document.getElementById("operand3").textContent = operand3;
-    document.getElementById("operand4").textContent = operand4;
-    document.getElementById("operator2").textContent = "-";
-}
-
-function showMulQuestion1(operand3, operand4) {
-    document.getElementById("operand3").textContent = operand3;
-    document.getElementById("operand4").textContent = operand4;
-    document.getElementById("operator2").textContent = "*";
-}
-
-function showDivQuestion1(operand3, operand4) {
-    document.getElementById("operand3").textContent = operand3;
-    document.getElementById("operand4").textContent = operand4;
-    document.getElementById("operator2").textContent = "/";
+function showQuestion1(question1) {
+    document.getElementById("question1").textContent = question1;
+   
 }
