1913f4e86712a52ca89e299b0392647ac4f78d70
Tobias Kister <Tobias.Kister@accenture.com>
2021-03-10 09:46:28 +0100

    Global Score

--- src/quiz/JS-JSON/quizFirebase.js
+++ src/quiz/JS-JSON/quizFirebase.js
@@ -4,7 +4,7 @@ let Quiz ={
         firebase.database().ref(`${this.qid}/`).once("value").then(function(sn) {
             var question=sn.val().question;
             var id = sn.val().id;
-            document.getElementById(`question`).innerHTML=question;
+            document.getElementById(`question:${QID}`).innerHTML=question;
             let ans = [];
             firebase.database().ref(`${QID}/answers`).once("value").then(function(snapshort) {
                 snapshort.forEach(snap => {
@@ -18,39 +18,73 @@ let Quiz ={
                         </div>`
                     );
                 });
-                document.getElementById('quiz-options').innerHTML = ans.join('');
+                document.getElementById(`quiz-options:${QID}`).innerHTML = ans.join('');
             });
         });
-        Quiz.getUserAnswer();
-        Quiz.showResult(this.qid);
-    },
-    getUserAnswer: function(){
-        //Getting the Value of the User Choosen Answer 
-        var asnwer = document.getElementById('quiz').elements['question'];
-        for (var i = 0; i < asnwer; i++) {
+        function getUserAnswer(){
+            //Getting the Value of the User Choosen Answer 
+            var asnwer = document.getElementById(`quiz:${QID}`).elements['question'];
+            for (var i = 0; i < asnwer; i++) {
                asnwer[i].onclick = function() {
                return asnwer.value;
-            };
-        };  
+                };
+            };  
+        };
+        getUserAnswer();
+        Quiz.showResult(this.qid);
+
     },
     showResult: function(qid){
-        alert("I'm here")
         //get final Entered Answer via URL
         let urlParams =  new URLSearchParams(document.location.search.substring(1));
         let UserAnswer= urlParams.get(`q${qid}`);
         firebase.database().ref(`${qid}/`).once("value").then(function(sn) {
             var rA=sn.val().rightAnswer;
-            //Check for correctness
+            //Check for correctnes
             if(UserAnswer){
                 if(UserAnswer == rA){
                     //What happens after the correct answer is given?
-                    Quiz.build(qid++); 
+                    alert("Right Answer")
+
                 }else{
                     //What happens after a wrong anser is given?
-                    alert("Wrong Answer");
-                }
-            }
+                    console.log("Wrong Answer");
+                };
+            };
+            //Update Global Scoreboard
+            Quiz.globalScore((UserAnswer == rA));
         });
+            
+    },
+    globalScore: function(correctAnswer){    
+        const questionsPerQuiz = 1;
+         //Abrufen, addieren und hinzufügen der Statistiken
+        firebase.database().ref("question").get().then( function(snapshot) {
+            if (snapshot.exists()) {
+              console.log(snapshot.val());
+              var data = snapshot.val();
+              var globalCountCorrect;
 
+              if (correctAnswer) {
+                globalCountCorrect = data.questionsCorrect + 1;
+                  }
+              else {
+                globalCountCorrect = data.questionsCorrect + 0;
+                  }
+              var globalCountQuestion = data.questionsAll + 1;
+              var globalPeopleCount = globalQuestionCount/questionsPerQuiz;
+            
+              firebase.database().ref('question').set(
+                {
+                questionsCorrect: globalCountCorrect,
+                questionsAll: globalCountQuestion,
+                questionsAverage: globalCountCorrect/globalPeopleCount
+                    }
+                  );
+                }
+                else {
+                  console.log("No data available");
+                }});
     }
-}
+};
+Quiz.build();
